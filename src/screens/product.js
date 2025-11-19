b719d985e91d9cd25fbbcdd3bd44bad1c36bd614
VITOR SANTOS PEREIRA <vitor_pereira153@estudante.sesisenai.org.br>
2025-11-19 15:21:02 -0300

    DROP_001

---
+++ src/screens/product.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+import { TouchableOpacity, View, Text, FlatList, ScrollView } from "react-native";
+import { StyleSheet } from "react-native";
+
+export default function Products() {
+
+
+
+return(
+
+    <View>
+        
+    </View>
+
+
+)
+}
